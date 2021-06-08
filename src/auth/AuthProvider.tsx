4c6867ad6935553f8b918cb39e7e3899199895c6
tact-kim <takuto.kimura.631604@gmail.com>
2021-06-08 22:37:22 +0900

    0608

---
+++ src/auth/AuthProvider.tsx
@@ -0,0 +1,32 @@
+import firebase from 'firebase/app'
+import { FC, createContext, useEffect, useState } from 'react'
+import { auth } from '../utils/firebase'
+
+type User = firebase.User;
+
+type AuthContextProps = {
+  currentUser: User | null | undefined
+}
+
+const AuthContext = createContext<AuthContextProps>({ currentUser: undefined })
+
+const AuthProvider: FC = ({children}) => {
+
+  const [currentUser, setCurrentUser] = useState<User | null | undefined>(
+      undefined
+    )
+    
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setCurrentUser(user)
+    })
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ currentUser }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export { AuthContext, AuthProvider }
