67831ccb608c053f379ab967fd437e9aef1d7d9d
tact-kim <takuto.kimura.631604@gmail.com>
2021-06-15 19:58:10 +0900

    phoneLink

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,35 @@
 import { useEffect, FC, useState } from 'react'
 import { useRouter } from 'next/router'
 // import Head from 'next/head'
+import firebase from 'firebase/app';
 
 import { auth } from '../utils/firebase'
 // import { route } from 'next/dist/next-server/server/router'
 
 const Home: FC = () => {
   const router = useRouter()
-  const [currentUser, setCurrentUser] = useState<null | object>(null)
+  const [currentUser, setCurrentUser] = useState<null | object>(null);
+  const [phoneNumber, setPhoneNumber] = useState< any >(null);
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       user ? setCurrentUser(user) : router.push('/login')
     })
   }, [])
+
+  const addPhoneNumber = async() => {
+    (window as any).recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptureID',{'size': 'invisible'});
+    const appVerifier = (window as any).recaptchaVerifier
+    const provider = new firebase.auth.PhoneAuthProvider();
+    const verificationId: any = provider.verifyPhoneNumber(phoneNumber, appVerifier);
+    try{
+      const code: any = prompt("Plz input verify code.");
+      const phoneCredential = firebase.auth.PhoneAuthProvider.credential(verificationId, code)
+      await auth.currentUser?.linkWithCredential(phoneCredential);
+    }catch(err){
+      alert(err.message);
+    }
+
+  }
 
   const logOut = async () => {
     try {
@@ -25,6 +42,23 @@ const Home: FC = () => {
   return (
     <div>
       <pre>{currentUser && JSON.stringify(currentUser, null, 4)}</pre>
+      <form onSubmit={addPhoneNumber}>
+        <div>
+          <label htmlFor="phone" className="auth-label">
+            Phone:{' '}
+          </label>
+          <input
+            id="phone"
+            className="auth-input"
+            type="tel"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          <div id="recaptureID"></div>
+          <button>
+            電話番号追加
+          </button>
+        </div>
+      </form>
       <button onClick={logOut}>Logout</button>
     </div>
   )
