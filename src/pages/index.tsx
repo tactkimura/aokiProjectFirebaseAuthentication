c9faf1926f7f0184d205c4347b9a2f7c41310df2
tact-kim <takuto.kimura.631604@gmail.com>
2021-07-06 19:44:53 +0900

    conectPhoneNumber

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 // import Head from 'next/head'
 
 import { auth } from '../utils/firebase'
+import firebase from 'firebase/app';
 // import { route } from 'next/dist/next-server/server/router'
 
 const Home: FC = () => {
@@ -13,6 +14,28 @@ const Home: FC = () => {
       user ? setCurrentUser(user) : router.push('/login')
     })
   }, [])
+
+  const addPhoneNumber = async (e: any) => {
+    e.preventDefault()
+    auth.settings.appVerificationDisabledForTesting = true;
+    const phoneNumber = prompt('Your Phone Number ?');
+    const code: any = prompt('Plz input your varify code.');
+    try {
+      (window as any).recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recapture-container');
+      const appVerifier = (window as any).recaptchaVerifier;
+      auth.signInWithPhoneNumber(`+81${phoneNumber}`, appVerifier)
+        .then((confirmationResult) => {
+          const credential = firebase.auth.PhoneAuthProvider.credential(confirmationResult.verificationId, code);
+          console.log(credential);
+          auth.currentUser?.linkWithCredential(credential);
+        }).catch((error) => {
+          (window as any).grecaptcha.reset((window as any).recaptchaWidgetId);
+          alert(error.message)
+        });
+    } catch(err){
+      alert(err.message);
+    }
+  }
 
   const logOut = async () => {
     try {
@@ -25,6 +48,8 @@ const Home: FC = () => {
   return (
     <div>
       <pre>{currentUser && JSON.stringify(currentUser, null, 4)}</pre>
+      <div id="recapture-container"></div>
+      <button onClick={addPhoneNumber}>Add Phone Number</button>
       <button onClick={logOut}>Logout</button>
     </div>
   )
